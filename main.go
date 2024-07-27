b75ac6fe2cb9830d3be0e0b4554280287531a5fd
Corax73 <ramaz999@inbox.ru>
2024-07-27 22:32:21 +0400

    work with methods

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 
@@ -13,6 +12,8 @@ import (
 
 type State struct {
 	Val1, Val2 int
+	Action     string
+	IsAction   bool
 }
 
 type Calc struct {
@@ -33,10 +34,10 @@ func main() {
 	input := widget.NewEntry()
 	input.SetPlaceHolder("Enter number")
 	btnSum := widget.NewButton("+", func() {
-		sumHandler(&c, c.Input, c.Display)
+		c.sumHandler(c.Input, c.Display)
 	})
 	btnSub := widget.NewButton("-", func() {
-		val1, err := strconv.Atoi(c.Input.Text)
+		/*val1, err := strconv.Atoi(c.Input.Text)
 		c.Input.SetText("0")
 		if err != nil {
 			fmt.Println(err)
@@ -49,7 +50,8 @@ func main() {
 			val2 = val1 * 2
 		}
 		res := strconv.Itoa(sub(val2, val1))
-		c.Display.SetText(res)
+		c.Display.SetText(res)*/
+		c.subHandler(c.Input, c.Display)
 	})
 
 	btnEquals := widget.NewButton("=", func() {
@@ -59,55 +61,90 @@ func main() {
 		c.Val2, err = strconv.Atoi(c.Input.Text)
 		var str string
 		if err == nil {
-			res := c.Val1 + c.Val2
-			str = strconv.Itoa(res)
+			switch c.Action {
+			case "+":
+				res := c.Val1 + c.Val2
+				str = strconv.Itoa(res)
+			case "-":
+				res := c.Val1 - c.Val2
+				str = strconv.Itoa(res)
+			default:
+				str = ""
+			}
 		} else {
 			str = err.Error()
 		}
+		c.resetState()
 		c.Input.SetText(str)
 	})
 
 	btnClear := widget.NewButton("Clear", func() {
 		c.Input.SetText("0")
+		c.Display.SetText("")
 	})
 
 	btnExit := widget.NewButton("Exit", func() {
 		myApp.Quit()
 	})
 	window.SetContent(
-		container.NewVBox(
+		container.NewGridWithColumns(
+			2,
 			c.Input,
-			c.addNumbBtn(1),
-			c.addNumbBtn(2),
-			c.addNumbBtn(3),
-			c.addNumbBtn(4),
-			c.addNumbBtn(5),
-			c.addNumbBtn(6),
-			c.addNumbBtn(7),
-			c.addNumbBtn(8),
-			c.addNumbBtn(9),
-			c.addNumbBtn(0),
-			btnSum,
-			btnSub,
-			btnEquals,
 			c.Display,
-			btnClear,
-			btnExit,
+			container.NewGridWithColumns(4,
+				c.addNumbBtn(1),
+				c.addNumbBtn(2),
+				c.addNumbBtn(3),
+				c.addNumbBtn(4)),
+			container.NewGridWithColumns(4,
+				c.addNumbBtn(5),
+				c.addNumbBtn(6),
+				c.addNumbBtn(7),
+				c.addNumbBtn(8)),
+			container.NewGridWithColumns(4,
+				c.addNumbBtn(9),
+				c.addNumbBtn(0),
+				btnSum,
+				btnSub),
+			container.NewGridWithColumns(3,
+				btnEquals,
+				btnClear,
+				btnExit),
 		),
 	)
-	window.Resize(fyne.NewSize(640, 180))
+	window.Resize(fyne.NewSize(300, 200))
 	window.ShowAndRun()
 }
 
-func sub(val2, val1 int) int {
-	return val2 - val1
-}
-
-func sumHandler(calc *Calc, input *widget.Entry, display *widget.Label) {
+func (calc *Calc) subHandler(input *widget.Entry, display *widget.Label) {
 	val, err := strconv.Atoi(input.Text)
 	if err != nil {
 		input.SetText(err.Error())
 	} else {
+		calc.IsAction = true
+		calc.Action = "-"
+		if calc.Val1 == 0 {
+			calc.Val1 = val
+			display.SetText("-")
+		} else {
+			calc.Val2 = val
+			equal := calc.Val1 - calc.Val2
+			res := strconv.Itoa(equal)
+			input.SetText(res)
+			display.SetText("")
+			calc.Val1 = 0
+			calc.Val2 = 0
+		}
+	}
+}
+
+func (calc *Calc) sumHandler(input *widget.Entry, display *widget.Label) {
+	val, err := strconv.Atoi(input.Text)
+	if err != nil {
+		input.SetText(err.Error())
+	} else {
+		calc.IsAction = true
+		calc.Action = "+"
 		if calc.Val1 == 0 {
 			calc.Val1 = val
 			display.SetText("+")
@@ -128,15 +165,22 @@ func (calc *Calc) addNumbBtn(number int) *widget.Button {
 	return widget.NewButton(str, func() {
 		val := calc.Input.Text
 		var newVal string
-		if val != "0" {
+		if val != "0" && !calc.IsAction {
 			var strBuilder strings.Builder
 			strBuilder.WriteString(val)
 			strBuilder.WriteString(str)
 			newVal = strBuilder.String()
 			strBuilder.Reset()
 		} else {
+			calc.IsAction = false
 			newVal = str
 		}
 		calc.Input.SetText(newVal)
 	})
+}
+
+func (calc *Calc) resetState() {
+	calc.Val1, calc.Val2 = 0, 0
+	calc.IsAction = false
+	calc.Action = ""
 }
